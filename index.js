2ed97c6f585a56bfecc575630866ec2830c61016
Ankit <black1314her@gmail.com>
2024-02-08 14:02:12 +0530

    Some New

---
+++ index.js
@@ -0,0 +1,42 @@
+// Primitive Data Types
+// Seven Types 
+// N N B B S S U
+// Null , Number , Bolean , BigInt, Symbol,String ,Undefined
+
+//  let a = 52;
+//  console.log(a, typeof a)
+
+//  let b = null;
+//  console.log(b ,typeof b)
+
+//  let ispass = true;
+//  console.log(ispass ,typeof ispass)
+
+//  let big = 456464646456465456465465465465465456465465446545646n;   
+//  console.log(big,typeof big)
+    
+// let name ="ritika" ;
+// console.log(name,typeof name)     
+
+// let j = undefined;
+// console.log(j,typeof j)
+  
+// const sym1 = Symbol();
+// console.log(sym1)
+
+
+
+
+
+// Non-Primitive Data Types
+
+// Object
+
+let data = {
+    "name" : "Ritika",
+    "role" : "Front-end Developer",
+    "salary": 15000
+}
+console.log(data)
+ data.gender="female"
+ console.log(data)
